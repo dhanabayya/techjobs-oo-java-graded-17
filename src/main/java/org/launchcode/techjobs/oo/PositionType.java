02c4433428c657f8c2117d825da3e5aa3e1182dc
Dhana bayya <krishnabayya@Dhanas-mac-Air.lan>
2024-11-10 18:53:12 -0600

    support classes done

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -17,10 +17,18 @@ public class PositionType {
     }
 
     // TODO: Add a custom toString() method that returns the data stored in 'value'.
+    public String toString() {
+        return value;
+    }
 
     // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
     //  their id fields match.
-
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PositionType)) return false;
+        PositionType that = (PositionType) o;
+        return id == that.id;
+    }
     // Getters and Setters:
 
     public int getId() {
