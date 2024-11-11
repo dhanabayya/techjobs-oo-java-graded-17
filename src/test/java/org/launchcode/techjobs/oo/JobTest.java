75550272d5d7aca6c08ccf11c929d7a6a9be62ee
Dhana bayya <krishnabayya@Dhanas-mac-Air.lan>
2024-11-11 11:43:27 -0600

    unit tests for job class

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,24 @@
 package org.launchcode.techjobs.oo;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class JobTest {
+    public int id;
+    Job job1=new Job();
+    Job job2=new Job();
     //TODO: Create your unit tests here
+    @Test
+    public void testSettingJobId(){
+        assertNotEquals(job1.getId(),job2.getId());
+    }
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        Job job=new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertTrue(job.getName(),true);
+        assertEquals(job.getEmployer().toString(),"ACME");
+        assertEquals(job.getLocation().toString(),"Desert");
+        assertEquals(job.getPositionType().toString(),"Quality control");
+        assertEquals(job.getCoreCompetency().toString(),"Persistence");
+    }
 }
